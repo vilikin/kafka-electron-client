7d66b23e320da10cc4899c233eeb70fe7d025282
Vili Kinnunen <vili.kinnunen1@gmail.com>
2020-05-17 18:01:43 +0300

    Add Tailwind support

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
 const fs = window.require("fs");
 
 function doNodeStuff() {
@@ -9,22 +7,16 @@ function doNodeStuff() {
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+    <div className="max-w-md mx-auto flex p-6 bg-gray-100 mt-10 rounded-lg shadow-xl">
+      <div className="ml-6 pt-1">
+        <h1 className="text-2xl text-blue-700 leading-tight">
+          Tailwind and Create React App
+        </h1>
+        <p className="text-base text-gray-700 leading-normal">
+          Building apps together
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React or not
-        </a>
-        <button onClick={doNodeStuff}>Lol</button>
-      </header>
+        <button onClick={doNodeStuff}>Do node stuff</button>
+      </div>
     </div>
   );
 }
