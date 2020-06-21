59c0076de496f892a8270c1cebae3e0bdc685b41
Vili Kinnunen <vili.kinnunen1@gmail.com>
2020-06-21 13:47:43 +0300

    Mock design for topic view header

--- src/components/Topic/Topic.tsx
+++ src/components/Topic/Topic.tsx
@@ -1,6 +1,15 @@
 import React, { FunctionComponent, useCallback } from "react";
 import { useEffects, useOvermindState } from "../../overmind";
 import { ConnectionStatus } from "../../overmind/connection/state";
+import {
+  FaPlay,
+  FaShare,
+  FaShareSquare,
+  FaTachometerAlt,
+  FaTrash,
+  FaAngleDoubleRight,
+  FaStop,
+} from "react-icons/fa";
 
 export interface TopicProps {
   topicName: string;
@@ -25,19 +34,43 @@ export const Topic: FunctionComponent<TopicProps> = ({ topicName }) => {
   }, [kafka, topicName]);
 
   return (
-    <div className="flex-1 flex h-full flex-col">
-      <p className="mt-8 text-lg text-gray-600 font-semibold">
-        Topic: {topic.id}
-      </p>
-      {!topic.consuming ? (
-        <button onClick={startConsuming} className="btn btn-primary">
-          Consume
+    <div className="flex-1 flex h-full flex-col p-2">
+      <h1 className="text-lg text-gray-700 font-semibold">{topic.id}</h1>
+      <div className="text-gray-600 mb-2">
+        Partitions: 2 | Replicas: 3 | Total records: 10 035 | Available records:
+        456
+      </div>
+      <div className="flex flex-row pb-3 border-b border-gray-200">
+        {topic.consuming ? (
+          <button
+            className="btn btn-secondary flex items-center mr-2"
+            onClick={stopConsuming}
+          >
+            <FaStop className="text-sm flex-auto flex-shrink-0 flex-grow-0" />
+            <span className="ml-2 text-md">Stop consuming</span>
+          </button>
+        ) : (
+          <button
+            className="btn btn-secondary flex items-center mr-2"
+            onClick={startConsuming}
+          >
+            <FaPlay className="text-sm flex-auto flex-shrink-0 flex-grow-0" />
+            <span className="ml-2 text-md">Start consuming</span>
+          </button>
+        )}
+        <button className="btn btn-secondary flex items-center mr-2" disabled>
+          <FaShareSquare className="text-sm flex-auto flex-shrink-0 flex-grow-0" />
+          <span className="ml-2 text-md">Produce</span>
         </button>
-      ) : (
-        <button onClick={stopConsuming} className="btn btn-primary">
-          Stop
+        <button className="btn btn-secondary flex items-center mr-2" disabled>
+          <FaTachometerAlt className="text-sm flex-auto flex-shrink-0 flex-grow-0" />
+          <span className="ml-2 text-md">Monitor</span>
         </button>
-      )}
+        <button className="btn btn-secondary flex items-center mr-2" disabled>
+          <FaAngleDoubleRight className="text-sm flex-auto flex-shrink-0 flex-grow-0" />
+          <span className="ml-2 text-md">Seek</span>
+        </button>
+      </div>
       <ul>
         {topic.records.map((record) => (
           <li key={record.topic + record.partition + record.offset}>
