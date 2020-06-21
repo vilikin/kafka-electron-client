7a38f1d4d71e4fc75cd9ee6144cf745d07667dbd
Vili Kinnunen <vili.kinnunen1@gmail.com>
2020-06-21 11:07:55 +0300

    Improve Sidebar styling

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,10 +1,45 @@
 import React, { FunctionComponent } from "react";
 import { useActions, useOvermindState } from "../overmind";
-import { ConnectionStatus } from "../overmind/connection/state";
+import {
+  ConnectionStatus,
+  KafkaTopicState,
+} from "../overmind/connection/state";
+import classNames from "classnames";
+import { PageId } from "../overmind/routing/state";
+
+interface TopicListProps {
+  topics: KafkaTopicState[];
+}
+
+export const TopicList: FunctionComponent<TopicListProps> = ({ topics }) => {
+  const { showTopicPage } = useActions().routing;
+  const { routing } = useOvermindState();
+
+  return (
+    <ul className="overflow-hidden mb-3 pt-1">
+      {topics.map((topic) => (
+        <li key={topic.id} className="px-1">
+          <button
+            onClick={() => showTopicPage(topic.id)}
+            className={classNames(
+              "w-full mb-1 py-2 px-2 text-left text-sm rounded-sm truncate break-all font-bold text-gray-600 cursor-pointer hover:bg-gray-200 focus:outline-none focus:shadow-outline",
+              {
+                "bg-gray-200 text-gray-700":
+                  routing.currentPageId === PageId.TOPIC &&
+                  routing.topicId === topic.id,
+              }
+            )}
+          >
+            {topic.id}
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 export const Sidebar: FunctionComponent = () => {
   const { state, topicList } = useOvermindState().connection;
-  const { showTopicPage } = useActions().routing;
 
   if (state.status !== ConnectionStatus.CONNECTED) {
     throw new Error("Can't render Sidebar while not being connected");
@@ -14,45 +49,19 @@ export const Sidebar: FunctionComponent = () => {
   const topicsNotBeingConsumed = topicList.filter((topic) => !topic.consuming);
 
   return (
-    <div className="sidebar border-gray-200 border-r py-3 px-2 overflow-y-scroll">
+    <div className="sidebar border-gray-200 border-r py-3 px-3 overflow-y-scroll">
       {topicsBeingConsumed.length > 0 && (
         <>
-          <h3 className="px-3 uppercase text-sm font-bold text-gray-600 mb-2">
+          <h3 className="uppercase text-sm font-bold text-gray-700 px-2 mb-1">
             Consuming
           </h3>
-          <ul className="overflow-hidden mb-3">
-            {topicsBeingConsumed.map((topic) => (
-              <li key={topic.id}>
-                <button
-                  onClick={() => showTopicPage(topic.id)}
-                  className="flex w-full rounded-sm py-2 px-3 cursor-pointer items-center hover:bg-gray-200 focus:outline-none focus:shadow-outline"
-                >
-                  <span className="text-sm font-bold text-gray-700 truncate break-all">
-                    {topic.id}
-                  </span>
-                </button>
-              </li>
-            ))}
-          </ul>
+          <TopicList topics={topicsBeingConsumed} />
         </>
       )}
-      <h3 className="px-3 uppercase text-sm font-bold text-gray-600 mb-2">
+      <h3 className="uppercase text-sm font-bold text-gray-700 px-2 mb-1">
         Topics
       </h3>
-      <ul className="overflow-hidden">
-        {topicsNotBeingConsumed.map((topic) => (
-          <li key={topic.id}>
-            <button
-              onClick={() => showTopicPage(topic.id)}
-              className="flex w-full rounded-sm py-2 px-3 cursor-pointer items-center hover:bg-gray-200 focus:outline-none focus:shadow-outline"
-            >
-              <span className="text-sm font-bold text-gray-700 truncate break-all">
-                {topic.id}
-              </span>
-            </button>
-          </li>
-        ))}
-      </ul>
+      <TopicList topics={topicsNotBeingConsumed} />
     </div>
   );
 };
