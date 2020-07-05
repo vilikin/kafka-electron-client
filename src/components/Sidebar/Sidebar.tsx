a06c09ea016c8b733ff885759ebefe14bbb7954d
Vili Kinnunen <vili.kinnunen1@gmail.com>
2020-07-05 19:58:46 +0300

    Keep sidebar topic & consumer group list ordering

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,13 +1,9 @@
 import React, { FunctionComponent } from "react";
 import { useActions, useOvermindState } from "../../overmind";
-import {
-  ConnectionStatus,
-  KafkaTopicState,
-} from "../../overmind/connection/state";
-import classNames from "classnames";
+import { ConnectionStatus } from "../../overmind/connection/state";
 import { PageId } from "../../overmind/routing/state";
-import { replaceEnvColor } from "../../util/tailwind-utils";
 import { CollapsableList } from "./CollapsableList";
+import * as _ from "lodash";
 
 export const Sidebar: FunctionComponent = () => {
   const { state, topicList, consumerGroupList } = useOvermindState().connection;
@@ -18,14 +14,26 @@ export const Sidebar: FunctionComponent = () => {
     throw new Error("Can't render Sidebar while not being connected");
   }
 
-  const topicsBeingConsumed = topicList.filter((topic) => topic.consuming);
-  const topicsNotBeingConsumed = topicList.filter((topic) => !topic.consuming);
+  const topicsBeingConsumed = _.chain(topicList)
+    .filter((topic) => topic.consuming)
+    .orderBy((topic) => topic.id)
+    .value();
+
+  const topicsNotBeingConsumed = _.chain(topicList)
+    .filter((topic) => !topic.consuming)
+    .orderBy((topic) => topic.id)
+    .value();
+
+  const consumerGroups = _.orderBy(
+    consumerGroupList,
+    (consumerGroup) => consumerGroup.id
+  );
 
   return (
     <div className="sidebar bg-gray-900 py-3 overflow-y-scroll">
       <CollapsableList
         header="Consumer groups"
-        items={consumerGroupList.map((consumerGroup) => ({
+        items={consumerGroups.map((consumerGroup) => ({
           id: consumerGroup.id,
           label: consumerGroup.id,
           selected:
@@ -59,44 +67,5 @@ export const Sidebar: FunctionComponent = () => {
         onItemClicked={(id) => showTopicPage(id)}
       />
     </div>
-  );
-};
-
-interface TopicListProps {
-  topics: KafkaTopicState[];
-}
-
-export const TopicList: FunctionComponent<TopicListProps> = ({ topics }) => {
-  const { showTopicPage } = useActions().routing;
-  const { routing, environments } = useOvermindState();
-
-  return (
-    <ul className="overflow-hidden mb-3 pt-1">
-      {topics.map((topic) => (
-        <li key={topic.id}>
-          <button
-            onClick={() => showTopicPage(topic.id)}
-            className={replaceEnvColor(
-              classNames(
-                "w-full py-2 px-6 text-left text-sm truncate break-all cursor-pointer focus:outline-none",
-                {
-                  "bg-envcolor-700 text-white":
-                    routing.currentPageId === PageId.TOPIC &&
-                    routing.topicId === topic.id,
-                },
-                {
-                  "text-gray-500 hover:bg-black focus:bg-black":
-                    routing.currentPageId !== PageId.TOPIC ||
-                    routing.topicId !== topic.id,
-                }
-              ),
-              environments.selectedEnvironment
-            )}
-          >
-            {topic.id}
-          </button>
-        </li>
-      ))}
-    </ul>
   );
 };
